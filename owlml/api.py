99b40a1f9194b4a23f86198017f24b6820fec9ea
sparrowml <ben@sparrow.dev>
2023-06-12 19:22:14 +0000

    require basic auth

--- owlml/api.py
+++ owlml/api.py
@@ -3,6 +3,7 @@ import os
 from typing import Any
 
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 def _get_required_env_var(env_var: str) -> str:
@@ -28,11 +29,14 @@ class OwlMLAPI:
 
     base_url: str = _get_required_env_var("OWLML_URL")
     api_url: str = os.path.join(base_url, "api")
+    basic_auth: HTTPBasicAuth = HTTPBasicAuth(
+        _get_required_env_var("OWLML_USERNAME"), _get_required_env_var("OWLML_PASSWORD")
+    )
 
     @classmethod
     def get(cls, route: str) -> dict[str, Any]:
         """Make a GET request to the OwlML API."""
-        response = requests.get(os.path.join(cls.api_url, route))
+        response = requests.get(os.path.join(cls.api_url, route), auth=cls.basic_auth)
         raise_for_status(response)
         if response.status_code == 204:
             return {}
@@ -41,7 +45,9 @@ class OwlMLAPI:
     @classmethod
     def post(cls, route: str, payload: dict[str, Any] = dict()) -> dict[str, Any]:
         """Make a POST request to the OwlML API."""
-        response = requests.post(os.path.join(cls.api_url, route), json=payload)
+        response = requests.post(
+            os.path.join(cls.api_url, route), auth=cls.basic_auth, json=payload
+        )
         raise_for_status(response)
         if response.status_code == 204:
             return {}
